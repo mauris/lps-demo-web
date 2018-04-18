d7fb2138d1d4c9c9b87157b5ee81ee727cd390e9
Sam-Mauris Yong <mauris@hotmail.sg>
2018-04-18 15:12:26 +0100

    update demo to show and load example programs

--- src/modules/app/pages/home.page.ts
+++ src/modules/app/pages/home.page.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title, Meta } from '@angular/platform-browser';
 import { Http, Headers, Response, RequestOptions } from '@angular/http';
 
 @Component({
   template: require('./home.page.pug')
 })
-export class HomePage {
+export class HomePage implements OnInit {
   private source: String;
   private timeline: Array<any> = [];
+  private preloadedPrograms: Array<String> = [];
 
   constructor(
     private title: Title,
     private http: Http,
     private meta: Meta
   ) {
+  }
+
+  ngOnInit() {
+    this.http
+      .get(process.env.API_ENDPOINT + '/lps/examples')
+      .map((response: Response) => {
+        let data = response.json();
+        this.preloadedPrograms = data.result;
+      })
+      .subscribe();
+  }
+
+  loadProgram(id: Number) {
+    this.http
+      .get(process.env.API_ENDPOINT + '/lps/examples/' + id)
+      .map((response: Response) => {
+        let data = response.json();
+        this.source = data.result;
+      })
+      .subscribe();
   }
 
   execute() {
