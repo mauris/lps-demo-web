5a778fece198eaf234f6cec89695576804d41d41
Sam-Mauris Yong <mauris@hotmail.sg>
2018-04-19 11:50:31 +0100

    implement transferState on homepage

--- src/modules/app/pages/home.page.ts
+++ src/modules/app/pages/home.page.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Title, Meta } from '@angular/platform-browser';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Title, Meta, TransferState, makeStateKey } from '@angular/platform-browser';
 import { Http, Headers, Response, RequestOptions } from '@angular/http';
+
+const RESULT_KEY = makeStateKey<Array<string>>('cache');
 
 @Component({
   template: require('./home.page.pug')
@@ -8,16 +11,27 @@ import { Http, Headers, Response, RequestOptions } from '@angular/http';
 export class HomePage implements OnInit {
   private source: String;
   private timeline: Array<any> = [];
-  private preloadedPrograms: Array<String> = [];
+  private preloadedPrograms: Array<string> = [];
 
   constructor(
     private title: Title,
     private http: Http,
-    private meta: Meta
+    private meta: Meta,
+    private transferState: TransferState,
+    @Inject(PLATFORM_ID) private platformId: any
   ) {
   }
 
   ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.preloadedPrograms = this.transferState.get(RESULT_KEY, []);
+      this.transferState.remove(RESULT_KEY);
+      return;
+    }
+
+    this.transferState.onSerialize(RESULT_KEY, () => {
+      return this.preloadedPrograms;
+    });
     this.http
       .get(process.env.API_ENDPOINT + '/lps/examples')
       .map((response: Response) => {
